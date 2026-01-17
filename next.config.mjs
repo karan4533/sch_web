f364582853d24cced34e47d053a953523d3be6a1
karan4533 <mlkaran2004@gmail.com>
2026-01-17 20:56:31 +0530

    fix: remove GH Pages base path for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  basePath: isProd ? "/E-learning" : "",
-  assetPrefix: isProd ? "/E-learning/" : "",
+  // basePath: isProd ? "/E-learning" : "",
+  // assetPrefix: isProd ? "/E-learning/" : "",
   output: "export",
   images: {
     unoptimized: true,
